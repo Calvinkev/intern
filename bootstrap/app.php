26a7846ab2084f04d031a5992f6e83c877f7d238
Calvinkev <calkev2004@gmail.com>
2026-07-29 02:32:39 -0400

    Initial Laravel framework setup

---
+++ bootstrap/app.php
@@ -0,0 +1,24 @@
+<?php
+
+use Illuminate\Foundation\Application;
+use Illuminate\Foundation\Configuration\Exceptions;
+use Illuminate\Foundation\Configuration\Middleware;
+
+return Application::configure(basePath: dirname(__DIR__))
+    ->withRouting(
+        web: __DIR__.'/../routes/web.php',
+        api: __DIR__.'/../routes/api.php',
+        commands: __DIR__.'/../routes/console.php',
+        health: '/up',
+    )
+    ->withMiddleware(function (Middleware $middleware): void {
+        $middleware->alias([
+            'customer' => \App\Http\Middleware\IsCustomer::class,
+            'restaurant.admin' => \App\Http\Middleware\IsRestaurantAdmin::class,
+            'delivery.personnel' => \App\Http\Middleware\IsDeliveryPersonnel::class,
+            'system.admin' => \App\Http\Middleware\IsSystemAdmin::class,
+        ]);
+    })
+    ->withExceptions(function (Exceptions $exceptions): void {
+        //
+    })->create();
